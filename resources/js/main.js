19434b909a453991068fa9aede2e3528dc350041
vesdim2007 <vesdim2007@gmail.com>
2018-05-29 12:24:42 +0300

    Commit project

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+  	slidesToshow: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    cssEase: 'linear',
+    draggable: false,
+    pauseOnSelect: false,
+    pauseOnHover: false,
+  });
+});
